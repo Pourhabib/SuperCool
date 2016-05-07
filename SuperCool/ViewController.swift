68e02ab943174573f0645d3882cc7773a34cf9a4
Pourhabib <siamakpourhabib@SIAMAKs-MacBook-Pro.local>
2016-05-06 21:56:01 -0700

    initial commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by SIAMAK POURHABIB on 2016-03-27.
+//  Copyright © 2016 Seiken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var getFitButton: UIButton!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func getFitandCool(sender: AnyObject) {
+        
+        logo.hidden = false
+        background.hidden = false
+        getFitButton.hidden = true
+        
+        
+        
+        
+    }
+    
+
+}
+
